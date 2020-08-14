0e46ea2ab0121ce24eeebd5d4dbc31c229d7973c
Montival <montival_junior@yahoo.com.br>
2020-08-14 20:24:56 -0300

    Criacao dos Models do projeto

---
+++ src/main/java/io/github/monthalcantara/estudoapicdc/detalhelivro/Categoria.java
@@ -0,0 +1,24 @@
+package io.github.monthalcantara.estudoapicdc.detalhelivro;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true)
+    private String nome;
+
+    @Deprecated
+    public Categoria() {
+    }
+
+    public Categoria(@NotBlank String nome) {
+        this.nome = nome;
+    }
+}
